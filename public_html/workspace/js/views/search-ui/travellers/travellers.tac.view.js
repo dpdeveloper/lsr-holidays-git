b0b487363bf9540fa0661d774adc16c176da07bf
David Anderson <dave@veodesign.co.uk>
2013-04-11 15:43:48 +0100

    Travellers Terms and Conditions View Added and interactions finished

--- public_html/workspace/js/views/search-ui/travellers/travellers.tac.view.js
+++ public_html/workspace/js/views/search-ui/travellers/travellers.tac.view.js
@@ -21,6 +21,28 @@ define([
 	{
 		initialize: function(options){
 			options = options || {};
+			this._status = this._lang.checkbox;
+		},
+		
+		_status: null,
+		
+		ui: {
+			items: '.tac-item',
+			status: 'a.tac-submit, .tac-inner',
+			msg: '.tac-msg',
+			submit: 'a.tac-submit'
+		},
+		
+		events: {
+			'click .tac-item': 'toggleCheckbox',
+			'click .tac-item input': 'refreshStatus',
+			'click a.tac-submit': 'handleSubmit'
+		},
+		
+		_lang: {
+			form: 'Please complete the forms above',
+			checkbox: 'Please agree to our terms and conditions',
+			go: 'Start your holiday today!'
 		},
 		
 		model: new Booking(),
@@ -32,8 +54,89 @@ define([
 			Serialize data for the view
 		*/
 		serializeData: function(){
-			return this.model.get('shortlistRequest').toJSON();
+			var data = this.model.get('shortlistRequest').toJSON();
+			data.status = this._status;
+			return data;
+		},
+		
+		/**
+		
+			onRender callback
+			
+		*/
+		onRender: function(){
+			this.refreshStatus();
+		},
+		
+		/**
+			Update the message in the UI
+		*/
+		updateMessage: function(){
+			this.ui.msg.text(this._status);
+		},
+		
+		/**
+			Handle a click on an item/checkbox and update the checkbox
+		*/
+		toggleCheckbox: function(ev){
+			ev.preventDefault();
+			
+			var $input = $(ev.currentTarget);
+			
+			if($input.prop('tagName')!=='input'){
+				$input = $input.find('input');
+				
+				if($input.is(':checked')){
+					$input.prop('checked',false);	
+				}
+				else{
+					$input.prop('checked',true);	
+				}
+			}
+			
+			this.refreshStatus();
+		},
+		
+		/**
+			Review the checkbox statuses and update the ui
+		*/
+		refreshStatus: function(){
+		
+			var checkedCount = 0;
+		
+			this.ui.items.each(function(index,elem){
+				if($(elem).find('input').is(':checked')){
+					$(elem).addClass('valid').removeClass('invalid');
+					checkedCount ++;
+				}
+				else{
+					$(elem).addClass('invalid').removeClass('valid');	
+				}
+			});
+			
+			if(checkedCount >= 2){
+				this._status = this._lang.go;
+				this.ui.status.addClass('valid').removeClass('invalid');
+			}
+			else{
+				this._status = this._lang.checkbox;		
+				this.ui.status.addClass('invalid').removeClass('valid');
+			}
+			this.updateMessage();
+		},
+		
+		/**
+			Handle the submit button being clicked
+		*/
+		handleSubmit: function(ev){
+			ev.preventDefault();
+			
+			if(this._status === this._lang.go){
+				vent.trigger('search:travellers:complete');	
+			}
 		}
+		
+		
 		
 	});
 	
--- tests/spec/views/search-ui/travellers/travellers.tac.view.spec.js
+++ tests/spec/views/search-ui/travellers/travellers.tac.view.spec.js
@@ -8,11 +8,12 @@ describe("TravellersTACView", function() {
 		$('#sandbox').show();
 		
 		require([
-			'backbone','marionette',
+			'backbone','marionette','vent',
 			'views/search-ui/travellers/travellers.tac.view'
-			], function(Backbone, Marionette, TravellersTACView) {
+			], function(Backbone, Marionette, vent, TravellersTACView) {
 
 			that.view = new TravellersTACView();
+			that.vent = vent;
 			
 			that.region = new Backbone.Marionette.Region({el: '#sandbox'});
 			flag = true;
@@ -32,5 +33,61 @@ describe("TravellersTACView", function() {
 		it('Is Not Null', function(){
 			expect(this.view).not.toBeNull();
 		});
+		it('Add to DOM', function(){
+			this.region.show(this.view);			
+			expect(this.view.$el).toBeVisible();
+		});
+	});
+	
+	describe('Interactions', function(){
+		
+		it('On click of tac-item, colour changes', function(){
+			this.region.show(this.view);
+			
+			expect(this.view.ui.items[0]).toHaveClass('invalid');
+			
+			$(this.view.ui.items[0]).trigger('click');
+			
+			expect(this.view.ui.items[0]).toHaveClass('valid');
+		});
+		
+		it('Once all checkboxes are ticked, the message changes', function(){
+			this.region.show(this.view);
+			$(this.view.ui.items).trigger('click');
+			
+			expect(this.view._status).toEqual(this.view._lang.go);
+			expect(this.view.ui.msg).toHaveText(this.view._lang.go);
+			expect(this.view.ui.status).toHaveClass('valid');
+		});
+		
+		it('If submit is clicked and disabled, nothing happens', function(){
+			this.region.show(this.view);
+			
+			var complete = false;
+			this.view.listenTo(this.vent,'search:travellers:complete',function(){
+				complete = true;
+			});
+			this.view.ui.submit.trigger('click');
+			expect(complete).toBeFalsy();
+			
+			
+		});
+		
+		it('If submit is clicked enabled, message fired', function(){
+			this.region.show(this.view);
+			
+			var complete = false;
+			this.view.listenTo(this.vent,'search:travellers:complete',function(){
+				complete = true;
+			});
+			
+			$(this.view.ui.items).trigger('click');
+			this.view.ui.submit.trigger('click');
+			
+			expect(complete).toBeTruthy();
+			
+			
+		});
+		
 	});
 });
